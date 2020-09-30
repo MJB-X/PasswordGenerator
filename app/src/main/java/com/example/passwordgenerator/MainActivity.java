553e8831715bd26599d0ffddde6de6f72baa45d7
MJBx-ux <monojb83@gmail.com>
2020-10-01 01:28:35 +0530

    new..... 1.password Save feature added 2.recyclerView and top menu implemented 3.nullpointExcption Fix

--- app/src/main/java/com/example/passwordgenerator/MainActivity.java
+++ app/src/main/java/com/example/passwordgenerator/MainActivity.java
@@ -1,24 +1,38 @@
 package com.example.passwordgenerator;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
+import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity {
-    EditText n,password;
+    EditText n,shwpassword;
     TextView label;
+    RecyclerView recyclerView;
+    myAdapter adapter;
+    String password="";
 
-    Button  generate;
+    Button  generate,save;
+    
 
 
     String[] genpassword=new String[20];
+    ArrayList<String> iteams=new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,13 +41,36 @@ public class MainActivity extends AppCompatActivity {
 
 
         initxml();
+
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+//        adapter=new myAdapter(fg);
+//        recyclerView.setAdapter(adapter);
         generate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                passGen();
-                String show= stringBuilder().toString();
-                password.setText(show);
-                label.setVisibility(View.VISIBLE);
+                if(TextUtils.isEmpty(n.getText().toString())) {
+                    Toast.makeText(MainActivity.this, "Enter length", Toast.LENGTH_SHORT).show();
+
+                }
+                else {
+                    passGen();
+                    password = stringBuilder().toString();
+                    shwpassword.setText(password);
+                    label.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                iteams.add(shwpassword.getText().toString());
+                adapter=new myAdapter(iteams);
+                recyclerView.setAdapter(adapter);
+
+
             }
         });
 
@@ -44,18 +81,38 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void passGen() {
-        int value=Integer.parseInt(n.getText().toString());
-        int count=0;
-        genpassword=new String[value];
-        Random r=new Random();
-        for(int i=0;i<value/2;i++){
-            String N=Integer.toString(r.nextInt(10));
-            genpassword[count++]=N;
-            String C=Character.toString((char)(r.nextInt(20)+'a'));
-            genpassword[count++]=C;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return  true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int selected = item.getItemId();
+        if(selected==R.id.settingmenu){
+            Toast.makeText(this, "selectMenu is selected", Toast.LENGTH_SHORT).show();
 
         }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void passGen() {
+
+
+
+            Integer value=Integer.parseInt(n.getText().toString());
+            int count = 0;
+            genpassword = new String[value];
+            Random r = new Random();
+            for (int i = 0; i < value / 2; i++) {
+                String N = Integer.toString(r.nextInt(10));
+                genpassword[count++] = N;
+                String C = Character.toString((char) (r.nextInt(20) + 'a'));
+                genpassword[count++] = C;
+
+            }
 
     }
 
@@ -72,8 +129,11 @@ public class MainActivity extends AppCompatActivity {
 
     private void initxml() {
         n=findViewById(R.id.inputN);
-        password=findViewById(R.id.password);
+        shwpassword=findViewById(R.id.password);
         label=findViewById(R.id.textView);
         generate=findViewById(R.id.btngenerate);
+        recyclerView=findViewById(R.id.recyclerView);
+        save=findViewById(R.id.save);
+
     }
 }
